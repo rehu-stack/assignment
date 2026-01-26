bad5505c0990b3d908d162608793fd37f56e046c
rehu-stack <rehulakdawala123@gmail.com>
2026-01-26 16:01:56 +0530

    First commit of task2

---
+++ task1.py
@@ -0,0 +1,8 @@
+#task-1
+num_1=float(input("enter the first number : "))
+num_2=float(input("enter the second number : "))
+
+print("addition = ", num_1 + num_2)
+print("substraction = ", num_1 - num_2)
+print("multiplication = ", num_1 * num_2)
+print("divition = ", num_1 /num_2)
