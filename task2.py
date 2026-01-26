b5b8ea5c6fdd55f67ab55db799f11b147c892954
rehu-stack <rehulakdawala123@gmail.com>
2026-01-26 16:52:06 +0530

    finished the setup

--- task2.py
+++ task2.py
@@ -5,3 +5,5 @@ first_name= input("enter your first name : ")
 last_name= input("enter your last name : ")
 full_name= first_name + " " + last_name
 print(f"Hello, {full_name}! welcome to the python program  ")
+
+#checking this 
