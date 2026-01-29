ad0a4a20ce8a1f294519577ee9efcdb843db7dc5
rehu-stack <rehulakdawala123@gmail.com>
2026-01-29 09:12:14 +0530

    first task of assignment-1

--- task1.py
+++ task1.py
@@ -1,4 +1,4 @@
-#task-1
+#task-1 of assignment 1
 num_1=float(input("enter the first number : "))
 num_2=float(input("enter the second number : "))
 
@@ -6,3 +6,4 @@ print("addition = ", num_1 + num_2)
 print("substraction = ", num_1 - num_2)
 print("multiplication = ", num_1 * num_2)
 print("divition = ", num_1 /num_2)
+
